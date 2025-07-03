dbd68b66f42b26b03f66d82b79e39a6295998c9e
Huỳnh Nguyên <140372018+nguyenhcp2004@users.noreply.github.com>
2025-07-03 22:22:50 +0700

    feat: new game room schema

--- src/game/schema/ChatSchema.ts
+++ src/game/schema/ChatSchema.ts
@@ -1,43 +1,69 @@
 import { Schema, type, MapSchema } from '@colyseus/schema'
 
-// export class Player extends Schema {
-//   @type('string') sessionId: string = ''
-//   @type('string') name: string = ''
-//   @type('number') joinedAt: number = 0
-//   @type('boolean') isOnline: boolean = true
-// }
-
-// export class ChatRoomState extends Schema {
-//   @type({ map: Player }) players: MapSchema<Player> = new MapSchema<Player>()
-//   @type('number') maxPlayers: number = 50
-//   @type('number') createdAt: number = 0
-//   @type('string') roomName: string = 'Chat Room'
-//   @type('boolean') isActive: boolean = true
-// }
-export class Player extends Schema {
-  @type('string')
-  sessionId: string = ''
-  @type('string')
-  name: string = ''
-  @type('boolean')
-  isOnline: boolean = true
+export class Pet extends Schema {
+  @type('string') id: string = ''
+  @type('string') ownerId: string = ''
+  @type('number') x: number = 0
+  @type('number') y: number = 0
+  @type('number') speed: number = 50
+  @type('string') currentActivity: string = 'walk'
+  @type('number') hungerLevel: number = 100
+  @type('boolean') isChasing: boolean = false
+  @type('number') targetX: number = 0
+  @type('number') targetY: number = 0
+  @type('string') petType: string = 'chog'
+  @type('number') lastFedAt: number = 0
+  @type('number') lastHungerUpdate: number = 0
 
   constructor() {
     super()
+    this.lastHungerUpdate = Date.now()
+  }
+}
+
+export class FoodItem extends Schema {
+  @type('string') id: string = ''
+  @type('string') foodType: string = ''
+  @type('number') quantity: number = 1
+  @type('number') price: number = 0
+  @type('number') x: number = 0
+  @type('number') y: number = 0
+  @type('string') droppedBy: string = ''
+  @type('number') droppedAt: number = 0
+
+  constructor() {
+    super()
+    this.droppedAt = Date.now()
+  }
+}
+
+export class Player extends Schema {
+  @type('string') sessionId: string = ''
+  @type('string') name: string = ''
+  @type('number') joinedAt: number = 0
+  @type('boolean') isOnline: boolean = true
+  @type({ map: FoodItem }) foodInventory: MapSchema<FoodItem> =
+    new MapSchema<FoodItem>()
+  @type('number') tokens: number = 100 // Game currency
+
+  constructor() {
+    super()
+    this.joinedAt = Date.now()
   }
 }
 
 export class GameRoomState extends Schema {
-  @type({ map: Player })
-  players: MapSchema<Player> = new MapSchema<Player>()
-  @type('string')
-  roomName: string = 'Game Room'
-  @type('boolean')
-  isActive: boolean = true
-  @type('number')
-  playerCount: number = 0
+  @type({ map: Player }) players: MapSchema<Player> = new MapSchema<Player>()
+  @type({ map: Pet }) pets: MapSchema<Pet> = new MapSchema<Pet>()
+  @type({ map: FoodItem }) droppedFood: MapSchema<FoodItem> =
+    new MapSchema<FoodItem>()
+  @type('string') roomName: string = 'Pet Simulator Room'
+  @type('number') playerCount: number = 0
+  @type('boolean') isActive: boolean = true
+  @type('number') createdAt: number = 0
 
   constructor() {
     super()
+    this.createdAt = Date.now()
   }
 }
